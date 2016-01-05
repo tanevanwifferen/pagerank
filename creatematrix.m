b68020bd83e8afbd5508aac11112d007762451fb
Martijn Verbakel <mhverbakel@gmail.com>
2016-01-06 00:29:57 +0100

    asdjvaskljas

--- creatematrix.m
+++ creatematrix.m
@@ -2,14 +2,12 @@
 function [matrix, transitions] = creatematrix(nodes, edges)
     matrix = zeros(size(nodes,1));
     for n = 1:size(edges);
-        row = edges(n, [1,2]);
-        if row(1) ~= row(2);
-            matrix(row(2), row(1)) = 1;
+        edge = edges(n,:);
+        if edge(1) ~= edge(2);
+            matrix(nodes == edge(1), nodes == edge(2)) = 1;
         end;
     end;
-    div = sum(matrix);
-    div(div == 0) = 1; % Replace 0 by 1, because division by 0 gives NaN, but we want 0
-    transitions = matrix / diag(div);
+    transitions = createtransitions(matrix);
     
     %indegrees = sum(matrix);
     %outdegrees = sum(matrix,2);
